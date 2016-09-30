f47388fefd11d02ffc7fe98b8feadc55633f9e3e
LinKding <beninaoin@gmail.com>
2016-09-30 14:30:47 +0800

    update

--- kill_process.sh
+++ kill_process.sh
@@ -1,3 +1,8 @@
 ##批量杀死进程脚本
 ps_uid=$1
 kill -9 `ps aux|grep $ps_uid|awk '{print $2}'`
+
+#批量删除旧文件
+#场景：目录文件按时间排列，删除一些历史最旧的若干文件
+## ll -lut 文件按时间顺序排列，tail -2 取排列最尾（旧）文件。
+rm -rf `ll -lut|tail -2|awk '{print $9}'`
