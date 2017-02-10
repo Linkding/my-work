1f081314961714d66a68834436cca554edb17e67
LinKding <beninaoin@gmail.com>
2017-02-10 10:59:21 +0800

    update & upload
    update  install_mysql
    upload mysql5.6 yum config file

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,4 +1,5 @@
 wget -P /usr/local/src/  http://7xvj0k.com1.z0.glb.clouddn.com/mysql57-community-release-el6-9.noarch.rpm
 #此为mysql5.7
 rpm -vhi /usr/local/src/mysql57-community-release-el6-9.noarch.rpm
+wget -P /etc/yum.repos.d/ http://7xvj0k.com1.z0.glb.clouddn.com/mysql-community.repo
 yum install  mysql-community-server -y
