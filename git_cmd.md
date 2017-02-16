178ae4af570ad774442de28a728f6c44734d680b
LinKding <beninaoin@gmail.com>
2017-02-16 11:22:24 +0800

    update

---
+++ git_cmd.md
@@ -0,0 +1,8 @@
+* 添加修改的文件到暂存区（stage或者叫index）
+git add main.py
+
+* 把暂存区的所有内容提交到当前分支
+git commit -m "update main.py"
+
+* 推送到远程主仓库
+git push origin master
--- install_mysql.sh
+++ install_mysql.sh
@@ -4,3 +4,11 @@ rpm -vhi /usr/local/src/mysql57-community-release-el6-9.noarch.rpm
 rm -rf /etc/yum.repos.d/mysql-community.repo
 wget -P /etc/yum.repos.d/ http://7xvj0k.com1.z0.glb.clouddn.com/mysql-community.repo
 yum install  mysql-community-server -y
+
+create table nei_pb_sort (Date date,codenumber int(6),name varchar(20),Close decimal(5,2), bookvalue decimal(5,2),PB decimal(5,3))
+Date,Codenumber,Name,bookvalue,changerate,Close,PB
+create table hk_pb_sort (Date date,Codenumber int(6),Name varchar(20),bookvalue decimal(5,2), changerate decimal(5,2),Close decimal(5,2), PB decimal(5,3))
+
+
+load data local infile '/lin/easybankPB/NeiBank/nei_pb.csv' into table nei_pb_sort fields terminated by "," IGNORE 1 LINES;
+load data local infile '/lin/easybankPB/NeiBank/hk_pb.csv' into table hk_pb_sort fields terminated by "," IGNORE 1 LINES;
